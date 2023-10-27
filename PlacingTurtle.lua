f1db0503bacf14ba5d73632076644be32018f47c
TryMyWagon <109018787+TryMyWagon@users.noreply.github.com>
2023-10-28 01:38:33 +1100

    sssssssssssssss

--- PlacingTurtle.lua
+++ PlacingTurtle.lua
@@ -1,4 +1,9 @@
-turtle.select(1)
-turtle.placedown()
-turtle.dropDown(64)
-sleep(10)
+local function Main()
+    turtle.select(1)
+    turtle.placedown()
+    turtle.dropDown(64)
+    sleep(10)
+end
+
+
+Main()

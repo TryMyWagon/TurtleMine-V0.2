603f35ff7668bef5d80e6124fa7fca6d5445dc50
TryMyWagon <109018787+TryMyWagon@users.noreply.github.com>
2023-10-28 02:13:19 +1100

    444444444444444444444

--- PlacingTurtle.lua
+++ PlacingTurtle.lua
@@ -1,7 +1,9 @@
 local function Main()
+    local computer = peripheral.wrap('down')
     turtle.select(1)
     turtle.placeDown()
     turtle.dropDown(64)
+    computer.turnOn()
     sleep(10)
 end
 Main()

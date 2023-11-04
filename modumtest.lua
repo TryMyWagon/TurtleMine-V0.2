0478fcf33419372427a89c6c8ff91eca2f554443
TryMyWagon <109018787+TryMyWagon@users.noreply.github.com>
2023-11-04 23:36:37 +1100

    sssssssssssssss

--- modumtest.lua
+++ modumtest.lua
@@ -1,12 +1,12 @@
-for i = 1, 16 do 
+for i = 1, 16 do
     local modem = peripheral.wrap("left")
     modem.open(3)  -- Open channel 3 so that we can listen on it
-    event, modemSide, senderChannel, 
+    local event, modemSide, senderChannel, 
       replyChannel, message, senderDistance = os.pullEvent("modem_message")
     Quandale = "Quandale"
     if Quandale == message then
         while true do
-           turtle.turnLeft() 
+           turtle.turnLeft()
         end
     end
     sleep(5)

c78e95a9f3ab3d0002d8796fa92ef7a80e220f4b
TryMyWagon <109018787+TryMyWagon@users.noreply.github.com>
2023-11-05 13:49:58 +1100

    sssssssssss

--- modumtest.lua
+++ modumtest.lua
@@ -1,3 +1,5 @@
+
+--[[
 for i = 1, 16 do
     local modem = peripheral.wrap("left")
     modem.open(3)  -- Open channel 3 so that we can listen on it
@@ -13,4 +15,16 @@ for i = 1, 16 do
     end
     sleep(5)
 end
+]]--
 
+local function cum(name)
+  if name == "pretzel" then
+    while true do
+      turtle.turnLeft()
+    end
+  else
+    print("lack of pretzelsk")
+  end
+end
+
+cum("real")
